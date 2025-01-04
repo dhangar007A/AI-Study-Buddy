c6db7d431cb1b4e77041dcadfe432722116b367c
adityapandey13-yours <aditya811560@gmail.com>
2025-01-04 18:57:21 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Banner from './components/banner';
+import ProfilePage from './components/ProfilePage'; // Import the ProfilePage component
 
 function App() {
-  return (<>
-  <Navbar />
-  <Banner />
-  </>);
+  return (
+    <Router>
+      <Navbar />
+      {/* Define routes here */}
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
