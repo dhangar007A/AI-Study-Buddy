e96448df3b4e851aa65c4fe118517e575c58ce65
dhangar007A <147794851+dhangar007A@users.noreply.github.com>
2025-01-04 18:53:47 +0530

    ...
    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const navItems = (
     <>
       <li><Link to="/">Home</Link></li>
-      <li><Link to="/only-for-you">OnlyForYou</Link></li>
+      <li><Link to="/onlyFans">OnlyForYou</Link></li>
       <li><Link to="/contact">Contact</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/profile">Profile</Link></li>
